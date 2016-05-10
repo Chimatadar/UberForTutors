2c7200cb2a8ffc495725a46a6769d6bbef1b130d
Chimatadar <chimatadarshivakiran@gmail.com>
2016-05-10 01:51:44 -0700

    Night Changes

--- UberForTutors/src/Controller/SignupController.java
+++ UberForTutors/src/Controller/SignupController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,6 +35,13 @@ public class SignupController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+		String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String location = request.getParameter("location");
+        String language = request.getParameter("language");
 	}
 
 }
--- UberForTutors/src/Model/UserModel.java
+++ UberForTutors/src/Model/UserModel.java
@@ -1,5 +1,5 @@
 package Model;
 
 public class UserModel {
-
+	public int UserId;
 }
