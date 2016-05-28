49f17629927b3578a4e583d8d50e47de012b8cbb
Chimatadar <chimatadarshivakiran@gmail.com>
2016-05-27 23:52:31 -0700

    sdf

--- UberForTutors/src/Model/SkillsLearntWithActivityId.java
+++ UberForTutors/src/Model/SkillsLearntWithActivityId.java
@@ -5,8 +5,5 @@ public class SkillsLearntWithActivityId {
 	public int tutorId;
 	public int activityId;
 	public String tutorEmail;
-<<<<<<< HEAD
-	
-=======
->>>>>>> 3fdb94ae9cd9f471489a832c0c993f71977529ad
+
 }
